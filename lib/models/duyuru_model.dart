0c6ec3b558cf1bcb9547415c51c06a029eecee30
emirhalici <emir.halici1@gmail.com>
2022-03-30 01:29:35 +0300

    add model file

---
+++ lib/models/duyuru_model.dart
@@ -0,0 +1,7 @@
+class DuyuruModel {
+  String title;
+  int day, month, year;
+  String text;
+  String url;
+  DuyuruModel({required this.title, required this.day, required this.month, required this.year, required this.text, required this.url});
+}
