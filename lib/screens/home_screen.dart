e6dff677de64338e7baab9ae43b2479b38e9450b
emirhalici <emir.halici1@gmail.com>
2022-03-31 00:06:47 +0300

    add notification to project

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:auduyurucu/components/duyuru_widget.dart';
 import 'package:auduyurucu/models/duyuru_model.dart';
 import 'package:auduyurucu/providers/main_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +10,17 @@ class HomeScreen extends StatefulWidget {
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
+}
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+showNotifications(String title, String body) async {
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+      AndroidNotificationDetails('id01', 'duyuruChannel', importance: Importance.max, priority: Priority.high, ticker: 'ticker');
+  const IOSNotificationDetails iosNotificationDetails = IOSNotificationDetails();
+  const NotificationDetails platformChannelSpecifics =
+      NotificationDetails(android: androidPlatformChannelSpecifics, iOS: iosNotificationDetails);
+  await flutterLocalNotificationsPlugin.show(0, title, body, platformChannelSpecifics, payload: 'item x');
 }
 
 class _HomeScreenState extends State<HomeScreen> {
